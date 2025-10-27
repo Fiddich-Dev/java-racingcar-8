c8051933f78842d0ee65ad44245a318373296711
Fiddich <hiws99@naver.com>
2025-10-27 22:59:19 +0900

    feat(test): junit을 사용해 테스트 코드 작성

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,12 +1,20 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import racingcar.parser.Parser;
+import racingcar.parser.PositiveBigInteger;
+import racingcar.parser.RaceParser;
+import racingcar.simulator.RaceSimulator;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.List;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
@@ -32,41 +40,61 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 경주_진행_및_우승자_테스트() {
+        // given
+        List<String> carNames = Arrays.asList("car1", "car2", "car3");
+        BigInteger count = BigInteger.valueOf(5);
+
+        // when
+        RaceSimulator simulator = new RaceSimulator(carNames, count);
+        List<String> winners = simulator.race();
+
+        // then
+        Assertions.assertFalse(winners.isEmpty());
+    }
+
+    @Test
     void 입력값_공백_테스트() {
-        assertRandomNumberInRangeTest(
-                () -> {
-                    run("  pobi ,  woni  ", "1");
-                    assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
-                },
-                MOVING_FORWARD, STOP
-        );
+        // given
+        String input = " car1 , car2  , car3  ";
+
+        // when
+        Parser<List<String>> parser = new RaceParser();
+        List<String> carNames = parser.parseToList(input);
+
+        // then
+        Assertions.assertEquals(3, carNames.size());
     }
 
     @Test
     void 자동차_이름_겹침_테스트() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("  pobi,  pobi ", "1"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
+        // given
+        String input = "car1,car1,car3";
+        Parser<List<String>> parser = new RaceParser();
+
+        // when, then
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            parser.parseToList(input);
+        });
     }
 
     @Test
-    void 횟수_0_테스트() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("  pobi,  pobi ", "0"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
+    void 양의정수_입력_테스트() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            PositiveBigInteger.of("0");
+        });
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            PositiveBigInteger.of("-10");
+        });
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            PositiveBigInteger.of("asd");
+        });
+
+        BigInteger bigInteger = PositiveBigInteger.of("10").getValue();
+        Assertions.assertEquals(bigInteger, BigInteger.valueOf(10));
     }
-
-    @Test
-    void 횟수_음수_테스트() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("  pobi,  pobi ", "-1"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
-    }
-
-
 
     @Override
     public void runMain() {
