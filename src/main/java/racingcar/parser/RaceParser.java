5fbaba175387f32d1a5ebc2834c1954ceb5abd78
Fiddich <hiws99@naver.com>
2025-10-27 22:17:37 +0900

    feat(parser): 자동차 이름이 겹칠때 예외처리 추가

--- src/main/java/racingcar/parser/RaceParser.java
+++ src/main/java/racingcar/parser/RaceParser.java
@@ -1,11 +1,22 @@
 package racingcar.parser;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 public class RaceParser extends Parser<List<String>> {
 
     private static final String INPUT_DELIMITER = ",";
     private static final String OUTPUT_DELIMITER = ", ";
+
+    @Override
+    public List<String> parseToList(String input) {
+        List<String> carNames = super.parseToList(input);
+        if(carNames.size() != new HashSet<>(carNames).size()) {
+            throw new IllegalArgumentException("자동차 이름이 겹치면 안됩니다.");
+        }
+        return carNames;
+    }
 
     @Override
     public String parseToOutputTemplate(List<String> result) {
